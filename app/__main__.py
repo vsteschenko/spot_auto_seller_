9c28ec14e267a90d8c7eb72f64de95c77e08aaca
Anton <109826223+Arcadiyyyyyyyy@users.noreply.github.com>
2023-09-19 19:38:29 +0100

    chore: remove debug statements

--- app/__main__.py
+++ app/__main__.py
@@ -1,13 +1,10 @@
 from src.config import Settings
-from src.binance_talker import get_tickers_price
 from dotenv import load_dotenv
 
 
 def main():
     load_dotenv()
     Settings()
-
-    print(get_tickers_price(["BTCUSDT", "BNBUSDT"]))  # Testing purposes TODO: remove
 
 
 if __name__ == "__main__":
