8ce32a94e1b8817b4128c444a7e4f05bbaaed983
Anton <109826223+Arcadiyyyyyyyy@users.noreply.github.com>
2023-09-19 17:39:12 +0100

    feat: implement setting of pairs to auto-sell

--- app/src/binance_talker.py
+++ app/src/binance_talker.py
@@ -1,3 +1,10 @@
 from binance import Client
+from os import environ
 
 
+def check_spot_balance():
+    api_public = environ.get("BINANCE_API_PUBLIC")
+    api_secret = environ.get("BINANCE_API_SECRET")
+    c = Client(api_public, api_secret)
+
+    
